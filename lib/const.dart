490b35a373a338190437781a73c55a9ad4bf27ff
Ahmed Allam <ahmed.allam@izam.com>
2026-08-24 19:30:55 +0300

    Add MyHome widget with app bar and image display

---
+++ lib/const.dart
@@ -0,0 +1,2 @@
+const image1 =
+    "https://img.magnific.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?semt=ais_hybrid&w=740&q=80";
--- lib/my_home.dart
+++ lib/my_home.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
+import 'const.dart';
+import 'home_widgets.dart';
+
 class MyHome extends StatelessWidget {
   const MyHome({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(color: Colors.red));
+    return Scaffold(
+      body: Column(
+        children: [
+          myAppBar(),
+          SizedBox(height: 40),
+          myImage(),
+          SizedBox(height: 30),
+
+          Text(
+            "Ahmed Allam",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10),
+          Text(
+            "Flutter Developer",
+            style: TextStyle(fontSize: 20, color: Colors.blue),
+          ),
+        ],
+      ),
+    );
   }
 }
