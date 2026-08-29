fdc41eaaa48501a59adf9356b894975607b6ac72
Ahmed Allam <ahmed.allam@izam.com>
2026-08-29 17:18:00 +0300

    scaffold parts

--- lib/my_home.dart
+++ lib/my_home.dart
@@ -9,83 +9,14 @@ class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        elevation: 5,
-        title: Text(
-          "My Profile",
-          style: TextStyle(
-            fontSize: 24,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+      appBar: AppBar(backgroundColor: Colors.amber),
+      body: Container(color: Colors.red),
+      bottomNavigationBar: Container(height: 200, color: Colors.blue),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Icon(Icons.add),
       ),
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          children: [
-            SizedBox(height: 40),
-            myImage(),
-            SizedBox(height: 30),
-
-            Text(
-              "Ahmed Allam",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text(
-              "Flutter Developer",
-              style: TextStyle(fontSize: 20, color: Colors.blue),
-            ),
-            SizedBox(height: 20),
-            Container(
-              height: 200,
-              width: 300,
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.blue.withOpacity(.1),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Column(
-                spacing: 10,
-                children: [
-                  Row(
-                    spacing: 10,
-                    children: [
-                      CircleAvatar(
-                        backgroundColor: Colors.blue.withOpacity(.1),
-                        child: Icon(
-                          Icons.person_2_outlined,
-                          color: Colors.blue,
-                        ),
-                      ),
-
-                      Text(
-                        "Welcome to my profile",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Divider(color: Colors.blue.withOpacity(.2)),
-                  Text(
-                    """
-I'm learning flutter I'm learning 
-I'm learning flutter I'm learning 
-I'm learning flutter I'm learning 
-                      """,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+   
     );
   }
 }
