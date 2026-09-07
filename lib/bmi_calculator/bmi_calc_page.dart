f8f5d947d4cc77b054c59fdbc4d194efea74f2bf
Ahmed Allam <ahmed.allam@izam.com>
2026-09-07 20:27:36 +0300

    bmi (2)

--- lib/bmi_calculator/bmi_calc_page.dart
+++ lib/bmi_calculator/bmi_calc_page.dart
@@ -12,6 +12,8 @@ class BmiCalcPage extends StatefulWidget {
 class _BmiCalcPageState extends State<BmiCalcPage> {
   Type type = Type.Male;
   double height = 158;
+  int weight = 0;
+  int age = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -34,101 +36,228 @@ class _BmiCalcPageState extends State<BmiCalcPage> {
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
-          spacing: 30,
+          spacing: 20,
           children: [
-            Row(
-              spacing: 20,
-              children: [
-                /// 2x + 2x = 4
-                /// x = 1
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      type = Type.Male;
-                      setState(() {});
-                    },
-                    child: Container(
-                      height: 190,
-                      decoration: BoxDecoration(
-                        color: type == Type.Male
-                            ? Colors.red
-                            : Colors.black.withOpacity(.7),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Male",
-                          style: TextStyle(fontSize: 30, color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      type = Type.Female;
-                      setState(() {});
-                    },
-                    child: Container(
-                      height: 190,
-                      decoration: BoxDecoration(
-                        color: type == Type.Female
-                            ? Colors.red
-                            : Colors.black.withOpacity(.7),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Female",
-                          style: TextStyle(fontSize: 30, color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Container(
-              height: 250,
-              width: double.infinity,
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Colors.black.withOpacity(.7),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Column(
+            Expanded(
+              flex: 2,
+              child: Row(
+                spacing: 20,
                 children: [
-                  Text(
-                    "Height",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                  Row(
-                    mainAxisAlignment: .center,
-                    children: [
-                      Text(
-                        height.toInt().toString(),
-                        style: TextStyle(fontSize: 80, color: Colors.white),
+                  /// 2x + 2x = 4
+                  /// x = 1
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        type = Type.Male;
+                        setState(() {});
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: type == Type.Male
+                              ? Colors.red
+                              : Colors.black.withOpacity(.7),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Male",
+                            style: TextStyle(fontSize: 30, color: Colors.white),
+                          ),
+                        ),
                       ),
-                      Text(
-                        "cm",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
-                      ),
-                    ],
+                    ),
                   ),
-                  Slider(
-                    thumbColor: Colors.red,
-                    activeColor: Colors.red,
-                    min: 70,
-                    max: 220,
-                    value: height,
-                    onChanged: (value) {
-                      print("Value is $value");
-                      height = value;
-                      setState(() {});
-                    },
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        type = Type.Female;
+                        setState(() {});
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: type == Type.Female
+                              ? Colors.red
+                              : Colors.black.withOpacity(.7),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Female",
+                            style: TextStyle(fontSize: 30, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 ],
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(.7),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  children: [
+                    Text("Height", style: _titleStyle()),
+                    Row(
+                      mainAxisAlignment: .center,
+                      children: [
+                        Text(height.toInt().toString(), style: _numberStyle()),
+                        Text(
+                          "cm",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    Slider(
+                      thumbColor: Colors.red,
+                      activeColor: Colors.red,
+                      min: 70,
+                      max: 220,
+                      value: height,
+                      onChanged: (value) {
+                        print("Value is $value");
+                        height = value;
+                        setState(() {});
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Expanded(
+              flex: 3,
+              child: Row(
+                spacing: 20,
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(.7),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Column(
+                        children: [
+                          Text("WEIGHT", style: _titleStyle()),
+                          Spacer(),
+
+                          Text(weight.toString(), style: _numberStyle()),
+
+                          Spacer(),
+                          Row(
+                            children: [
+                              FloatingActionButton(
+                                onPressed: () {
+                                  weight++;
+                                  setState(() {});
+                                },
+                                backgroundColor: Colors.grey,
+                                shape: CircleBorder(),
+                                child: Icon(Icons.add),
+                              ),
+                              Spacer(),
+                              FloatingActionButton(
+                                onPressed: () {
+                                  weight--;
+                                  setState(() {});
+                                },
+                                backgroundColor: Colors.grey,
+                                shape: CircleBorder(),
+                                child: Icon(Icons.remove),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(.7),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Column(
+                        children: [
+                          Text("Age", style: _titleStyle()),
+                          Spacer(),
+
+                          Text(age.toString(), style: _numberStyle()),
+                          Spacer(),
+                          Row(
+                            children: [
+                              FloatingActionButton(
+                                onPressed: () {
+                                  age++;
+                                  setState(() {});
+                                },
+                                backgroundColor: Colors.grey,
+                                shape: CircleBorder(),
+                                child: Icon(Icons.add),
+                              ),
+                              Spacer(),
+                              FloatingActionButton(
+                                onPressed: () {
+                                  age--;
+                                  setState(() {});
+                                },
+                                backgroundColor: Colors.grey,
+                                shape: CircleBorder(),
+                                child: Icon(Icons.remove),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                var result = "";
+                double bmi = (weight) / (height * height / 10000);
+                if (bmi < 18.5) {
+                  result = "Underweight";
+                } else if (bmi < 24.9) {
+                  result = "Healthy weight";
+                } else if (bmi < 29.9) {
+                  result = "Overweight";
+                } else {
+                  result = "Obese";
+                }
+                
+
+                // show the dialog
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("BMI Result"),
+                      content: Text("Your Bmi is ${bmi.toInt()} you are $result"),
+                    );
+                  },
+                );
+              },
+              child: Container(
+                height: 80,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Center(
+                  child: Text("Check Your BMI", style: _titleStyle()),
+                ),
               ),
             ),
           ],
@@ -136,4 +265,8 @@ class _BmiCalcPageState extends State<BmiCalcPage> {
       ),
     );
   }
+
+  TextStyle _numberStyle() => TextStyle(fontSize: 80, color: Colors.white);
+
+  TextStyle _titleStyle() => TextStyle(color: Colors.white, fontSize: 20);
 }
