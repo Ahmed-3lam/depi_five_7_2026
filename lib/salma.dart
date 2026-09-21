56169de5e15b2564daab93f5721213f22e371e8a
Ahmed Allam <ahmed.allam@izam.co>
2026-09-21 19:54:44 +0300

    add column

--- lib/salma.dart
+++ lib/salma.dart
@@ -1,13 +1,15 @@
-void main() {
-  runApp(const MyApp());
-}
+
+
+import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(home: NotePage());
+    return Scaffold(
+      body: Column(),
+    );
 
 
 
